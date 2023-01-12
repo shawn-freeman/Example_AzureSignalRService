989eaa63ebac250b062f164e7cd0d776745c94bc
Shawn Freeman <sfreeman@sparksgroupinc.com>
2023-01-12 13:10:16 -0500

    Add Azure Signal R Connection String to app settings

---
+++ AzureSignalRServiceCoreApi/AzureSignalRServiceCoreApi/Hubs/DashboardHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace AzureSignalRServiceCoreApi.Hubs
+{
+    public class DashboardHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
