903c7d1f1c6a479b4dca85758b22d952d5e0cf82
Shawn Freeman <sfreeman@sparksgroupinc.com>
2023-01-12 14:26:36 -0500

    DotNet48 owin.cors and signal R

---
+++ AzureSignalRServiceCoreApi/AzureSignalRServiceDotNet48Api/Startup.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.Owin;
+using Microsoft.Owin.Cors;
+using Owin;
+using System;
+using System.Threading.Tasks;
+
+[assembly: OwinStartup(typeof(AzureSignalRServiceDotNet48Api.Startup))]
+
+namespace AzureSignalRServiceDotNet48Api
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+
+            app.Map("/signalr", map =>
+            {
+                // Setup the CORS middleware to run before SignalR.
+                // By default this will allow all origins. You can 
+                // configure the set of origins and/or http verbs by
+                // providing a cors options with a different policy.
+                map.UseCors(CorsOptions.AllowAll);
+                var hubConfiguration = new HubConfiguration
+                {
+                    // You can enable JSONP by uncommenting line below.
+                    // JSONP requests are insecure but some older browsers (and some
+                    // versions of IE) require JSONP to work cross domain
+                    // EnableJSONP = true
+                };
+
+                //map.MapAzureSignalR(this.GetType().FullName);
+                map.RunAzureSignalR(this.GetType().FullName,
+                    "",
+                    hubConfiguration);
+
+
+                //map.UseCors(_corsOptions.Value);
+                // Run the SignalR pipeline. We're not using MapSignalR
+                // since this branch already runs under the "/signalr"
+                // path.
+                //map.RunSignalR(hubConfiguration);
+            });
+            //app.MapAzureSignalR(this.GetType().FullName);
+        }
+    }
+}
