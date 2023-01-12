0bed841634c81187eac73ae1d287d0b21d9d8de0
Shawn Freeman <sfreeman@sparksgroupinc.com>
2023-01-12 13:25:01 -0500

    Add signal R connection code

--- angular_signalr_client/src/app/app.component.ts
+++ angular_signalr_client/src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+const signalR = require("@microsoft/signalr");
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular_signalr_client';
+
+  
+
+  constructor(){
+
+  }
+
+  ngOnInit(): void {
+    let connection = new signalR.HubConnectionBuilder()
+        .withUrl("/dashboardHub")
+        .build();
+
+    connection.on("send", data => {
+        console.log(data);
+    });
+
+    connection.start()
+        .then(() => connection.invoke("send", "Hello"));
+  }
 }
